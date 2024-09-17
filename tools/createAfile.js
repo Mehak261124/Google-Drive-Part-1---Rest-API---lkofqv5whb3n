98ee1ab888c2944e0058ac5c862c3be781cc8611
Mehak Jain <145124929+Mehak261124@users.noreply.github.com>
2024-09-17 14:39:29 +0530

    Update createAfile.js

--- tools/createAfile.js
+++ tools/createAfile.js
@@ -1,9 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
-const createAFile = (fileName, fileData) => {
-  const filePath = path.join(__dirname, '../root', fileName);
-  fs.writeFileSync(filePath, fileData, 'utf8');
+import fs from "fs";
+const createAfile = (fileName, fileData) => {
+  fs.writeFileSync(`./root/${fileName}`, fileData);
 };
-
-module.exports = createAFile;
+export default createAfile;
